e415fe8b0b1348d91285d84a3cfb6b3de211edfe
Imran Farhat <farhatimran724@gmail.com>
2026-07-30 11:31:32 +0000

    Code challenge submission for question: 1979 Find Greatest Common Divisor of Array

---
+++ 1979 Find Greatest Common Divisor of Array/1979find-greatest-common-divisor-of-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findGCD = function(nums) {
+    let min = Math.min(...nums);
+    let max = Math.max(...nums);
+
+    while (min !== 0) {
+        let temp = min;
+        min = max % min;
+        max = temp;
+    }
+
+    return max;
+};
