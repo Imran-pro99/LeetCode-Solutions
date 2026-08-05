400cb8f1d3c794bd9acb9070431a7f6ed8397d2c
Imran Farhat <farhatimran724@gmail.com>
2026-08-05 10:06:00 +0000

    Code challenge submission for question: 1528 Shuffle String

---
+++ 1528 Shuffle String/1528shuffle-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    let shuffle = [];
+    for(let i=0; i<s.length; i++){
+        shuffle[indices[i]] = s[i];
+    }
+    return shuffle.join("");
+};
